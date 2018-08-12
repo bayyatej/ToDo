ed406461ce610d19a768790f48ee3700d7f3a19f
Teja Bayya <bayyatej.dev@gmail.com>
2018-08-12 19:22:01 -0400

    add callback to populate database with dummy data

--- ToDo/app/src/main/java/com/geterdone/android/todo/TaskRoomDatabase.java
+++ ToDo/app/src/main/java/com/geterdone/android/todo/TaskRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.geterdone.android.todo;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {Task.class}, version = 1)
 public abstract class TaskRoomDatabase extends RoomDatabase
@@ -11,6 +14,36 @@ public abstract class TaskRoomDatabase extends RoomDatabase
 	public abstract TaskDao taskDao();
 
 	private static TaskRoomDatabase INSTANCE;
+	private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback()
+	{
+		@Override
+		public void onOpen(@NonNull SupportSQLiteDatabase db)
+		{
+			super.onOpen(db);
+			new PopulateDbAsync(INSTANCE).execute();
+		}
+	};
+
+	private static class PopulateDbAsync extends AsyncTask<Void, Void, Void>
+	{
+		private final TaskDao mDao;
+
+		PopulateDbAsync(TaskRoomDatabase db)
+		{
+			mDao = db.taskDao();
+		}
+
+		@Override
+		protected Void doInBackground(final Void... params)
+		{
+			mDao.deleteAll();
+			Task task = new Task("Crush it", "24/7");
+			mDao.insert(task);
+			task = new Task("Wheel, Snipe, Celly", "Ferda");
+			mDao.insert(task);
+			return null;
+		}
+	}
 
 	public static TaskRoomDatabase getDatabase(final Context context)
 	{
@@ -21,7 +54,8 @@ public abstract class TaskRoomDatabase extends RoomDatabase
 				if (INSTANCE == null)
 				{
 					INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-													TaskRoomDatabase.class, "task_database").build();
+													TaskRoomDatabase.class, "task_database")
+							.addCallback(sRoomDatabaseCallback).build();
 				}
 			}
 		}
