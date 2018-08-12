a057608f9f0d54821c7f18fc5c7cacb45b08598f
Teja Bayya <bayyatej.dev@gmail.com>
2018-08-12 18:58:19 -0400

    hook up adapter to recycler view

--- ToDo/app/src/main/java/com/geterdone/android/todo/MainActivity.java
+++ ToDo/app/src/main/java/com/geterdone/android/todo/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -15,10 +17,10 @@ public class MainActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		FloatingActionButton fab = findViewById(R.id.fab);
 		fab.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
@@ -28,6 +30,11 @@ public class MainActivity extends AppCompatActivity
 						.setAction("Action", null).show();
 			}
 		});
+
+		RecyclerView recyclerView = findViewById(R.id.recyclerview);
+		final TaskListAdapter adapter = new TaskListAdapter(this);
+		recyclerView.setAdapter(adapter);
+		recyclerView.setLayoutManager(new LinearLayoutManager(this));
 	}
 
 }
